90f812c1ad1c5b893d52e1d3b669b33fd7344b90
Mehulsain111 <mehulsain555@gmail.com>
2025-12-24 15:34:10 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,36 @@
 import "./App.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'; 
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { FC, useEffect } from "react";
+import Lenis from "lenis";
+
 import SignUp from "./component/SignUp";
-import { FC } from "react";
 import { Navbar } from "./Navbar";
 import { Herosection } from "./component/Herosection";
 import { Login } from "./component/Login";
 import { MiddleSection } from "./component/MiddleSection";
 import { FooterSection } from "./component/FooterSection";
+
 const App: FC = () => {
+  useEffect(() => {
+    const lenis = new Lenis({
+      lerp: 0.1,
+      smoothWheel: true,
+    });
+
+    const raf = (time: number) => {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    };
+
+    requestAnimationFrame(raf);
+
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
+
   return (
     <div className="background">
       <BrowserRouter>
